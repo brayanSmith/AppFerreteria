fd2a9e64305dd616e4fad28a0d7ce3753b56f688
unknown <brayan.suarezsegura308@gmail.com>
2025-10-09 21:32:46 -0500

    Autorizaciones

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -62,5 +64,10 @@ class User extends Authenticatable
     public function abonos()
     {
         return $this->hasMany(Abono::class, 'user_id');
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
     }
 }
